f14a79b1655d0a0b554ebe0c09107f5c8b1dca6d
diegobshots <blancolopez4@gmail.com>
2020-03-03 22:32:43 +0100

    3/3 casa

--- ProyectoFInal/efectosJquery.js
+++ ProyectoFInal/efectosJquery.js
@@ -7,6 +7,34 @@
             });
             //entrada de imagen
             
+            $('.carousel').slick({
+                centerMode: true,
+                slidesToShow: 3,
+                // infinite: true,
+                speed: 400,
+                cssEase: 'linear',
+                adaptiveHeight: true,
+                responsive: [
+                  {
+                    breakpoint: 768,
+                    settings: {
+                      arrows: true,
+                      centerMode: true,
+                      centerPadding: '40px',
+                      slidesToShow: 3
+                    }
+                  },
+                  {
+                    breakpoint: 482,
+                    settings: {
+                      arrows: false,
+                      centerMode: true,
+                      centerPadding: '40px',
+                      slidesToShow: 1
+                    }
+                  }
+                ]
+              });
 
             
         });
