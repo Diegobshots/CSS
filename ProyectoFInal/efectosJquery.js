f7564d6f244976328a15642aa45c93b072694041
diegobshots <blancolopez4@gmail.com>
2020-03-03 20:20:12 +0100

    3/3 casa

---
+++ ProyectoFInal/efectosJquery.js
@@ -0,0 +1,12 @@
+
+        //Solución con jQUery
+        $(document).ready(function () {
+            $('.hamburger').click(function () {
+                $('.hamburger').toggleClass('is-active');
+                $('.menuppal').toggleClass('is-active');
+            });
+            //entrada de imagen
+            
+
+            
+        });
